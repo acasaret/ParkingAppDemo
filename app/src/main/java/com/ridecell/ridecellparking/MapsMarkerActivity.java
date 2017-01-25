5618093d9da2a962f29dafc86843022af3cc3600
Tony Casaretto <anthony.casaretto@gmail.com>
2017-01-24 23:34:51 -0800

    basic map app structure

---
+++ app/src/main/java/com/ridecell/ridecellparking/MapsMarkerActivity.java
@@ -0,0 +1,45 @@
+package com.ridecell.ridecellparking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsMarkerActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Retrieve the content view that renders the map.
+        setContentView(R.layout.activity_maps);
+        // Get the SupportMapFragment and request notification
+        // when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map when it's available.
+     * The API invokes this callback when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user receives a prompt to install
+     * Play services inside the SupportMapFragment. The API invokes this method after the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in SF
+        // and move the map's camera to the same location.
+        LatLng sf = new LatLng(37.773972, -122.431297);
+        googleMap.addMarker(new MarkerOptions().position(sf)
+                .title("Marker in SF"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sf));
+    }
+}
